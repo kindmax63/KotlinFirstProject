04f00727e330d26e64eee26303037d85fb6a1b61
KINDMAX <nic-key@mail.ru>
2024-06-12 21:34:45 +0400

    learning

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import com.sun.jdi.IntegerValue
+import java.util.Random
+
+const val HERO_NAME = "Kindmax"
+
+
+fun main () {
+  var playerLevel = 4
+    playerLevel ++
+    println(playerLevel)
+    println(HERO_NAME)
+    var hasSteed = ""
+    var tavernName = "Taverna"
+    var traktirshikName = "Sataru"
+    var wallet = 15
+    println(tavernName.reversed())
+
+    var randomNumber = kotlin.random.Random(100)
+
+if (randomNumber.nextInt() > 1) {
+  println("rtr")
+}  else {
+  println("kek")
+}
+
+}
