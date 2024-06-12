baf962ade88df49d17676ecaec265a7e5853fbd2
KINDMAX <nic-key@mail.ru>
2024-06-12 22:20:00 +0400

    learning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
 const val INFO = "text"
+//val - переменные только для чтения
+//var - переменные изменяемые
+//const val вне функции - переменная компиляции
+
+//котлин может сам умеет определять тип переменной, можно проверить через байт код Tools -> Kotlin REPL
+
+//переменные есть ссылочные и примитивные. Integer & int
+
+
 
 fun main () {
         println(INFO)
         var text = "string"
+
+    var angry = true
+    var happy = false
+    val everyday = !happy && !angry
+    val emotions: String = if (!happy) {
+        "yes, happy"
+    } else { "not today, unhappy"}
     }
 
