573bf9756691ddfde747b46211bb95d4383e140f
KINDMAX <nic-key@mail.ru>
2024-06-12 22:55:29 +0400

    learning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,38 @@ const val INFO = "text"
 //val - переменные только для чтения
 //var - переменные изменяемые
 //const val вне функции - переменная компиляции
-
-//котлин может сам умеет определять тип переменной, можно проверить через байт код Tools -> Kotlin REPL
-
+//котлин сам умеет определять тип переменной, можно проверить через байт код Tools -> Kotlin REPL
 //переменные есть ссылочные и примитивные. Integer & int
-
-
+// в котлин есть интервалы ranges
 
 fun main () {
-        println(INFO)
-        var text = "string"
+    println(INFO)
+    var text = "string"
+
+    var level = 1
 
     var angry = true
-    var happy = false
+    var happy = true
+    //можно объявлять переменную с условными выражениями &, ||, != и другими
     val everyday = !happy && !angry
     val emotions: String = if (!happy) {
         "yes, happy"
     } else { "not today, unhappy"}
+    println (emotions)
+
+    //интервалы в условиях
+    if (level in 1..5){
+        println("level less 5")
+    }
+    //until не берет верхнюю границу
+    if (level in 1 until 5){
+        println("level between 1 and 4")
+    }
+    //downTo
+    if (level in 1 downTo 5){
+        println("интервал с убывающими значениями - downTo")
+    }
+
+
     }
 
